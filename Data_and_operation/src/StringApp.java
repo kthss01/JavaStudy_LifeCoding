aadfbcdd0f58a345ea505015af94300c45b83234
김태훈 <kthss01@naver.com>
2020-06-18 22:35:50 +0900

    Java GUI Swing 가볍게 구경 Data and Operation, String 가볍게 공부

---
+++ Data_and_operation/src/StringApp.java
@@ -0,0 +1,24 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World"); // String
+//		System.out.println('Hello World'); // Character
+		System.out.println('H'); // Character
+		
+		System.out.println("Hello"
+				+ " World"); // ¡Ÿ πŸ≤ﬁ æ»µ 
+		/*
+		 * System.out.println("Hello" " World");
+		 */
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\""); // Hello "World"
+	}
+
+}
